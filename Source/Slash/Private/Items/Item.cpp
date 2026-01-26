6b64299b13b28537a6f00d38c2de9ca2717a19bc
NayeemHossenJim <nayeemhossenjim@gmail.com>
2026-01-26 22:08:28 +0600

    Draw Debug Lines

--- Source/Slash/Private/Items/Item.cpp
+++ Source/Slash/Private/Items/Item.cpp
@@ -1,5 +1,6 @@
 #include "Items/Item.h"
 #include "DrawDebugHelpers.h"
+#include "Slash/Slash.h"
 
 AItem::AItem()
 {
@@ -10,33 +11,17 @@ AItem::AItem()
 void AItem::BeginPlay()
 {
 	Super::BeginPlay();
-	UE_LOG(LogTemp, Warning, TEXT("Item Begin Play"));
-
-	if(GEngine)
-	{
-		GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Yellow, TEXT("Item Begin Play"));
-	}
 
 	UWorld* World = GetWorld();
-	if (World)
-	{
-		DrawDebugSphere(World, GetActorLocation(), 30.f, 12, FColor::Red, true, 10.f);
-	}
+
+	FVector Location = GetActorLocation();
+	FVector Forward = GetActorForwardVector();
+
+	DRAW_SPHERE(Location)
+	DRAW_LINE(Location, Location + Forward * 100.f)
 }
 
 void AItem::Tick(float DeltaTime)
 {
-	Super::Tick(DeltaTime);
-	UE_LOG(LogTemp, Warning, TEXT("DeltaTime: %f"), DeltaTime);
-
-	if (GEngine)
-	{
-		FString Name = GetName();
-		FString Message = FString::Printf(TEXT("Item Name: %s"), *Name);
-		GEngine->AddOnScreenDebugMessage(1, 60.f, FColor::Cyan, Message);
-
-		UE_LOG(LogTemp, Warning, TEXT("Item Name: %s"), *Name);
-	}
-
 
 }
