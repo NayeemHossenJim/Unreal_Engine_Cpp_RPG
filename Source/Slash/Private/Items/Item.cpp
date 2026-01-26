3426c9f56d6786c39e029e1fb8385dbc4c25fcbf
NayeemHossenJim <nayeemhossenjim@gmail.com>
2026-01-26 23:46:12 +0600

    Section 5 challenge complete Square Debug added

--- Source/Slash/Private/Items/Item.cpp
+++ Source/Slash/Private/Items/Item.cpp
@@ -1,5 +1,6 @@
 #include "Items/Item.h"
 #include "Slash/DebugMacros.h"
+#include "DrawDebugHelpers.h"
 
 AItem::AItem()
 {
@@ -17,6 +18,8 @@ void AItem::BeginPlay()
 
 	DRAW_SPHERE(Location);
 	DRAW_VECTOR(Location, Location + Forward * 100.f);
+	DRAW_BOX(Location, FVector(20.f));
+
 }
 
 void AItem::Tick(float DeltaTime)
